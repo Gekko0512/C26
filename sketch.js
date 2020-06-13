7655595e3611dc7ace16f2e88a543ee622be2678
Piyush Sahoo <Piyush.Sahoo@outlook.com>
2020-06-13 15:43:13 +0530

    platform1

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var platform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform1=new Ground(400,height,100,50);
+    
 
 }
 
@@ -61,4 +64,6 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    platform1.display(); 
 }
